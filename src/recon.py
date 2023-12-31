e1d50b2b3b20435bd6ebc277392dde7fed30abc7
Jeter Lin <jeter.lin1208@gmail.com>
2023-12-31 16:47:12 +0800

    added distance calculation for recon.py

--- src/recon.py
+++ src/recon.py
@@ -1,5 +1,6 @@
 import os
 import cv2
+import math
 import logging
 import numpy as np
 
@@ -14,6 +15,7 @@ def load_templete(path) -> list[str]:
 
 
 def matching(screenshot, templetes, threshold=0.8):
+    rlt:list[tuple] = []
     img_rgb = cv2.imread(screenshot)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
@@ -23,14 +25,37 @@ def matching(screenshot, templetes, threshold=0.8):
         w, h = pattern.shape[::-1]
         res = cv2.matchTemplate(img_gray, pattern, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
-        LOGGER.info(f'loc result: {loc}')
-        for pt in zip(*loc[::-1]):
-            LOGGER.info(f'coordinate: {pt}')
-            cv2.rectangle(img_rgb, pt, (pt[0] + w, pt[1] + h), (0, 0, 255), 2)
-            # cv2.circle(img_rgb, pt, radius=1, color=(0, 0, 255), thickness=-1)
+        possible_coordinate = [(int(c[0]+w/2), int(c[1]+h/2)) \
+                                                for c in zip(*loc[::-1])]
+        _append_only_diff_coordinates(possible_coordinate, rlt)
+
+    LOGGER.info(f'Possible Location for {screenshot.split("/")[-1]}: {rlt}')
+
+    for pt in rlt:
+        cv2.circle(
+            img=img_rgb,
+            center=pt,
+            radius=5,
+            color=(0, 0 ,255),
+            thickness=-1
+        )
 
     cv2.imwrite(screenshot.replace(".png", "_rlt.png"), img_rgb)
+    return rlt
 
+def _append_only_diff_coordinates(incomming_coor, rlt_coor):
+    for c in incomming_coor:
+        if len(rlt_coor) == 0:
+            rlt_coor.append(c)
+        else:
+            for r in rlt_coor:
+                if calculate_distance(c, r) < 5:
+                    break
+            else:
+                rlt_coor.append(c)
+
+def calculate_distance(x:list[int], y:list[int]) -> float:
+    return math.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
 
 if __name__ == "__main__":
     # matching(
@@ -38,6 +63,6 @@ if __name__ == "__main__":
     #     templete=r'images/coal/coal_2.png'
     # )
     for b in glob(r"images\benchmarks\*.png"):
-        matching(b, load_templete(TEMPLETE))
-        break
+        print(f'match rlt: {matching(b, load_templete(TEMPLETE))}')
+        
         # print(b)
