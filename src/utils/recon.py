cffb18b12aab94a186c4cc443be767b2e3047dbb
k1ngjet3r <jeter.lin1208@gmail.com>
2024-01-01 16:12:57 +0800

    relocated and modified recon and screenshot modules

--- src/utils/recon.py
+++ src/utils/recon.py
@@ -6,26 +6,30 @@ import numpy as np
 
 from glob import glob
 
+from utils.screenshot import ScreenGrabber
+
 LOGGER = logging.getLogger(__name__)
 logging.basicConfig(level=logging.DEBUG)
 TEMPLETE = r"images\coal"
+
 
 def load_templete(path) -> list[str]:
     return [f for f in glob(path + r"\*.png")]
 
 
-def matching(screenshot, templetes, threshold=0.8):
-    rlt:list[tuple] = []
+def matching(screenshot, templetes: list[str], threshold=0.8):
+    rlt: list[tuple] = []
     img_rgb = cv2.imread(screenshot)
     img_gray = cv2.cvtColor(img_rgb, cv2.COLOR_BGR2GRAY)
 
     for t in templetes:
-        LOGGER.debug(f'Seraching templete: {t}')
+        LOGGER.debug(f"Seraching templete: {t}")
         pattern = cv2.imread(t, 0)
         w, h = pattern.shape[::-1]
         res = cv2.matchTemplate(img_gray, pattern, cv2.TM_CCOEFF_NORMED)
         loc = np.where(res >= threshold)
-        possible_coordinate = [(int(c[0]+w/2), int(c[1]+h/2)) \
+        possible_coordinate = [
+            (int(c[0] + w / 2), int(c[1] + h / 2)) \
                                                 for c in zip(*loc[::-1])]
         _append_only_diff_coordinates(possible_coordinate, rlt)
 
@@ -33,14 +37,9 @@ def matching(screenshot, templetes, threshold=0.8):
 
     for pt in rlt:
         cv2.circle(
-            img=img_rgb,
-            center=pt,
-            radius=5,
-            color=(0, 0 ,255),
-            thickness=-1
-        )
+            img=img_rgb, center=pt, radius=5, color=(0, 0, 255), thickness=-1)
 
-    cv2.imwrite(screenshot.replace(".png", "_rlt.png"), img_rgb)
+    cv2.imwrite(screenshot, img_rgb)
     return rlt
 
 def _append_only_diff_coordinates(incomming_coor, rlt_coor):
@@ -54,15 +53,20 @@ def _append_only_diff_coordinates(incomming_coor, rlt_coor):
             else:
                 rlt_coor.append(c)
 
-def calculate_distance(x:list[int], y:list[int]) -> float:
-    return math.sqrt((x[0]-y[0])**2 + (x[1]-y[1])**2)
+def calculate_distance(x: list[int], y: list[int]) -> float:
+    return math.sqrt((x[0] - y[0]) ** 2 + (x[1] - y[1]) ** 2)
 
 if __name__ == "__main__":
     # matching(
     #     screenshot=r'images\benchmarks\benchmark_1.png',
     #     templete=r'images/coal/coal_2.png'
     # )
-    for b in glob(r"images\benchmarks\*.png"):
-        print(f'match rlt: {matching(b, load_templete(TEMPLETE))}')
-        
-        # print(b)
+    screenshot, center, _ = ScreenGrabber()
+    coal_location = matching(screenshot, load_templete(TEMPLETE))
+    relative_location = [
+        (center[0] - coal[0], center[1] - coal[1]) for coal in coal_location
+    ]
+
+    print(f"Relative location: {relative_location}")
+
+    # print(b)
--- src/utils/screenshot.py
+++ src/utils/screenshot.py
@@ -1,4 +1,5 @@
 import os
+from datetime import datetime
 import logging
 import pyautogui
 import pygetwindow as pgw
@@ -6,13 +7,14 @@ import pygetwindow as pgw
 from PIL import Image
 
 LOGGER = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
+# logging.basicConfig(level=logging.DEBUG)
 
 IMAGE_ROOT = "images"
 
 
 def ScreenGrabber():
-    image_name = os.path.join(IMAGE_ROOT, "test.png")
+    filename = datetime.now().strftime("%H_%M_%S") + ".png"
+    image_name = os.path.join(IMAGE_ROOT, filename)
     titles = pgw.getAllTitles()
     if "Heartwood Online" not in titles:
         LOGGER.error("Heartwood Online window cannot be found!")
@@ -25,11 +27,14 @@ def ScreenGrabber():
     pyautogui.screenshot(image_name)
     im = Image.open(image_name)
     im = im.crop((left, top, right, bottom))
+    # LOGGER.info(f"Screen Center: {left+10, top+10}")
     im.save(image_name)
+    center = ((left + right) / 2, (top + bottom) / 2)
+    window_top_left_corner = (left + 10, top + 10)
+    return image_name, center, window_top_left_corner
 
 
 if __name__ == "__main__":
-    image_name = os.path.join(IMAGE_ROOT, "test.png")
     ScreenGrabber()
-    im = Image.open(image_name)
-    im.show()
+    # im = Image.open(image_name)
+    # im.show()
