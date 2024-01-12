5c346447ff7c62c991823b9a5b0136bc013e75d3
Jeter Lin <jeter.lin1208@gmail.com>
2024-01-12 11:02:07 +0800

    renamed character control package

--- src/utils/character_ctrl.py
+++ src/utils/character_ctrl.py
@@ -1,22 +1,21 @@
 import time
-import logging
 import pyautogui
+from loguru import logger
 
 from utils.utils import load_mvmt_params, Coordinate
-
-LOGGER = logging.getLogger(__name__)
-logging.basicConfig(level=logging.DEBUG)
 
 PACE = load_mvmt_params()
 
 class Character_Ctrl:
     @staticmethod
     def _hold_and_release(key, duration):
+        logger.debug(f'holding {key} for duration: {duration}s')
         pyautogui.keyDown(key)
         time.sleep(duration)
         pyautogui.keyUp(key)
 
     def click_window(self, p:Coordinate):
+        logger.debug(f'Clicking window at {(p.x, p.y)}')
         pyautogui.click(p.x, p.y)
 
     def _move_horizonal(self, x, debug=False):
@@ -48,26 +47,28 @@ class Character_Ctrl:
                 self._hold_and_release(key, duration=0.5)
 
     def inventory(self):
+        logger.debug('Open/close inventory')
         pyautogui.press("b")
 
     def go_to_town(self):
+        logger.debug('Teleport back to the town')
         pyautogui.press("t")
 
     def mine(self, p:Coordinate):
-        LOGGER.debug("Mining...")
+        logger.debug("Mining...")
         pyautogui.click(p.x, p.y)
 
-    def move_to(self, p:Coordinate):
+    def move(self, p:Coordinate):
         self._move_vertical(p.y)
         self._move_horizonal(p.x)
 
 
 if __name__ == "__main__":
     Ctrl = Character_Ctrl()
-    Ctrl.click_window(7, 19)
+    # Ctrl.click_window(7, 19)
     # Ctrl.inventory()
     # time.sleep(3)
     # Ctrl.inventory()
     # time.sleep(1)
-    Ctrl._move_horizonal(-2)
-    time.sleep(1)
+    Ctrl.move(Coordinate(2, 3))
+
