9c47e53e117556a4ab893cb557881ace7cad1933
k1ngjet3r <jeter.lin1208@gmail.com>
2024-01-14 12:14:43 +0800

    added round half up when calculate the movement

--- src/utils/character_ctrl.py
+++ src/utils/character_ctrl.py
@@ -1,3 +1,4 @@
+import math
 import time
 import pyautogui
 from loguru import logger
@@ -6,16 +7,20 @@ from utils.utils import load_mvmt_params, Coordinate
 
 PACE = load_mvmt_params()
 
+def round_half_up(n, decimals=3):
+    multiplier = 10**decimals
+    return math.floor(n * multiplier + 0.5) / multiplier
+
 class Character_Ctrl:
     @staticmethod
     def _hold_and_release(key, duration):
-        logger.debug(f'holding {key} for duration: {duration}s')
+        logger.debug(f"holding {key} for duration: {duration}s")
         pyautogui.keyDown(key)
         time.sleep(duration)
         pyautogui.keyUp(key)
 
-    def click_window(self, p:Coordinate):
-        logger.debug(f'Clicking window at {(p.x, p.y)}')
+    def click_window(self, p: Coordinate):
+        logger.debug(f"Clicking window at {(p.x, p.y)}")
         pyautogui.click(p.x, p.y)
 
     def _move_horizonal(self, x, debug=False):
@@ -27,7 +32,7 @@ class Character_Ctrl:
                 key = "a"
                 pace = PACE.get("left")
             if not debug:
-                duration = abs(x / pace)
+                duration = round_half_up(abs(x / pace))
                 self._hold_and_release(key, duration)
             else:
                 self._hold_and_release(key, duration=0.5)
@@ -41,26 +46,29 @@ class Character_Ctrl:
                 key = "w"
                 pace = PACE.get("up")
             if not debug:
-                duration = abs(y / pace)
+                duration = round_half_up(abs(y / pace))
                 self._hold_and_release(key, duration)
             else:
                 self._hold_and_release(key, duration=0.5)
 
     def inventory(self):
-        logger.debug('Open/close inventory')
+        logger.debug("Open/close inventory")
         pyautogui.press("b")
 
     def go_to_town(self):
-        logger.debug('Teleport back to the town')
+        logger.debug("Teleport back to the town")
         pyautogui.press("t")
 
-    def mine(self, p:Coordinate):
+    def mine(self, p: Coordinate):
         logger.debug("Mining...")
         pyautogui.click(p.x, p.y)
 
-    def move(self, p:Coordinate):
+    def move(self, p: Coordinate):
         self._move_vertical(p.y)
         self._move_horizonal(p.x)
+
+    def press(self, key, presses):
+        pyautogui.press(key, presses=presses)
 
 
 if __name__ == "__main__":
@@ -70,5 +78,4 @@ if __name__ == "__main__":
     # time.sleep(3)
     # Ctrl.inventory()
     # time.sleep(1)
-    Ctrl.move(Coordinate(2, 3))
-
+    Ctrl.mine(Coordinate(645, 396))
