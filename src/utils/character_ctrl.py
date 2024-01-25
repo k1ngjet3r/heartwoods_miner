86319a092b79364f925ce50407164cd41ed8124c
k1ngjet3r <jeter.lin1208@gmail.com>
2024-01-25 23:04:23 +0800

    corrected the _calculate_mvmt formula

--- src/utils/character_ctrl.py
+++ src/utils/character_ctrl.py
@@ -28,7 +28,7 @@ class Character_Ctrl:
         pyautogui.click(p.x, p.y)
 
     def _calculate_mvmt(self, distance):
-        return abs(0.003326 * distance - 0.113757)
+        return abs(0.003326 * abs(distance) - 0.113757)
 
     def _move_horizonal(self, x, debug=False):
         if x != 0:
@@ -70,40 +70,68 @@ class Character_Ctrl:
         self._move_vertical(p.y)
         self._move_horizonal(p.x)
 
+    def move_reverse(self, p: Coordinate):
+        self._move_horizonal(p.x)
+        self._move_vertical(p.y)
+
     def press(self, key, presses=1):
         pyautogui.press(key, presses=presses)
 
     def heal(self):
-        logger.debug('Healing the character')
-        self.press('1')
+        logger.debug("Healing the character")
+        self.press("1")
 
     def go_to_bank(self):
-        logger.debug('Go to bank...')
+        logger.debug("Go to bank...")
         self.go_to_town()
-        self._move_horizonal
-        raise NotImplementedError('go to bank function is not ready yet')
+        time.sleep(0.5)
+        for m in [Coordinate(660, 0), Coordinate(0, -800)]:
+            self.move(m)
 
     def go_to_coal_spot_from_town(self):
-        logger.debug('Teleport back to town and go to coal spot...')
+        logger.debug("Teleport back to town and go to coal spot...")
         self.go_to_town()
         time.sleep(1)
         for m in [Coordinate(1280, 0), Coordinate(0, -720), Coordinate(640, 0)]:
             self.move(m)
 
+    def go_to_coal_spot_from_bank(self):
+        logger.debug("Heading to coal spot from bank...")
+        for m in [
+            Coordinate(0, 240),
+            Coordinate(640, 0),
+            Coordinate(0, -800),
+            Coordinate(600, 0),
+        ]:
+            self.move(m)
 
-# def ctrl(key, duration):
-#     exe = str(Path(r'C:\Users\Jeter\dev\heartwoods_miner\src\utils\keyboard_ctrl\keyboard_control_api.exe'))
-#     try:
-#         subprocess.run([exe, str(key), str(duration)], check=True)
-#         logger.debug(f'Holding key {key} for {duration} ms')
-#     except subprocess.CalledProcessError as e:
-#         logger.error(f'Error: {e}')
+    def click_bank(self):
+        logger.debug("Click the bank")
+        pyautogui.click()
+
+
+def ctrl(key, duration):
+    exe = str(
+        Path(
+            r"C:\Users\Jeter\dev\heartwoods_miner\src\utils\keyboard_ctrl\keyboard_control_api.exe"
+        )
+    )
+    try:
+        subprocess.run([exe, str(key), str(duration)], check=True)
+        logger.debug(f"Holding key {key} for {duration} ms")
+    except subprocess.CalledProcessError as e:
+        logger.error(f"Error: {e}")
+
 
 if __name__ == "__main__":
+    time.sleep(5)
     Ctrl = Character_Ctrl()
     # Ctrl.click_window(7, 19)
     # Ctrl.inventory()
     # time.sleep(3)
     # Ctrl.inventory()
     # time.sleep(1)
-    Ctrl.mine(Coordinate(645, 396))
+    # Ctrl.mine(Coordinate(645, 396))
+    # Ctrl.go_to_bank()
+    # time.sleep(0.5)
+    Ctrl.go_to_coal_spot_from_bank()
